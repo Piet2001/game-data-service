90c84029f170d5513d5b8ebd45817e24175e4825
Piet2001 <piet2001@fireman.net>
2021-04-08 15:16:39 +0200

    Initial commit

---
+++ src/main/java/com/firestationquiz/gamedataservice/GameDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.firestationquiz.gamedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDataServiceApplication.class, args);
+	}
+
+}
